47386d2a6b15b91f09681fb890e1a90913e39fdc
Roy Donasco <rdonasco2@yahoo.com>
2012-11-10 17:42:39 +0800

    added height and weight parameter

--- rdonasco-common-vaadin/src/main/java/com/rdonasco/common/vaadin/view/utils/EmbeddedResourceBuilder.java
+++ rdonasco-common-vaadin/src/main/java/com/rdonasco/common/vaadin/view/utils/EmbeddedResourceBuilder.java
@@ -17,7 +17,9 @@ package com.rdonasco.common.vaadin.view.utils;
 
 import com.rdonasco.common.exceptions.InvalidBuilderParameter;
 import com.vaadin.Application;
+import com.vaadin.terminal.Sizeable;
 import com.vaadin.terminal.StreamResource;
+import com.vaadin.terminal.ThemeResource;
 import com.vaadin.ui.Embedded;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
@@ -28,12 +30,17 @@ import java.io.InputStream;
  */
 public class EmbeddedResourceBuilder
 {
+
 	private byte[] bytes;
 	private String caption;
 	private String description;
 	private Application application;
 	private String name;
 	private String alternateText;
+	private String resourceId;
+	private Float height;
+	private int units = Sizeable.UNITS_PIXELS;
+	private Float width;
 
 	public EmbeddedResourceBuilder()
 	{
@@ -62,34 +69,67 @@ public class EmbeddedResourceBuilder
 		this.description = description;
 		return this;
 	}
-	
+
 	public EmbeddedResourceBuilder setName(String name)
 	{
 		this.name = name;
 		return this;
 	}
-	
+
 	public EmbeddedResourceBuilder setAlternateText(String alternateText)
 	{
 		this.alternateText = alternateText;
 		return this;
 	}
+	
+	public EmbeddedResourceBuilder setResourceId(String resourceId)
+	{
+		this.resourceId = resourceId;
+		return this;
+	}
+	
+	public EmbeddedResourceBuilder setHeight(float height, int units)
+	{
+		this.height = height;
+		this.units = units;
+		return this;
+	}
 
 	public Embedded createEmbedded() throws InvalidBuilderParameter
-	{		
-		if(null == bytes)
-		{
-			throw new InvalidBuilderParameter("bytes to build not set");
-		}
-		if(null == application)
+	{
+		if (null == application)
 		{
 			throw new InvalidBuilderParameter("application not set");
-		}	
-		if(null == name)
+		}
+		if (null == name)
 		{
 			throw new InvalidBuilderParameter("name not set");
 		}
+		Embedded embedded = null;
+		if(null != bytes)
+		{
+			embedded = createUsingStreamSource();
+		}
+		else if(null != resourceId)
+		{
+			embedded = createUsingResourceId();
+		}
+		embedded.setDescription(description);
+		embedded.setAlternateText(alternateText);
+		if(null != height)
+		{
+			embedded.setHeight(height, units);
+		}
+		if(null != width)
+		{
+			embedded.setWidth(width, units);
+		}
 		
+		return embedded;
+	}
+
+	private Embedded createUsingStreamSource()
+	{
 		StreamResource.StreamSource streamSource = new StreamResource.StreamSource()
 		{
 			private static final long serialVersionUID = 1L;
@@ -101,9 +141,13 @@ public class EmbeddedResourceBuilder
 			}
 		};
 		StreamResource resource = new StreamResource(streamSource, name, application);
-		Embedded embedded = new Embedded(caption,resource);
-		embedded.setDescription(description);
-		embedded.setAlternateText(alternateText);
+		Embedded embedded = new Embedded(caption, resource);
+
 		return embedded;
+	}
+
+	private Embedded createUsingResourceId()
+	{
+		return new Embedded(caption,new ThemeResource(resourceId));
 	}
 }
