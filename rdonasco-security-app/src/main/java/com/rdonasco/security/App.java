13443cb1915b20051c0d1d13183f03a900faa876
Roy Donasco <rdonasco2@yahoo.com>
2013-02-11 10:47:20 +0800

    added new application for testing CRUD of security entities

---
+++ rdonasco-security-app/src/main/java/com/rdonasco/security/App.java
@@ -0,0 +1,17 @@
+package com.rdonasco.security;
+
+import com.rdonasco.security.services.SystemSecurityManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	private SystemSecurityManager systemSecurityManager;
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
