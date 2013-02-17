332c55c12c189d4e86cf59ab3812852f4bf101d2
rdonasco <devnull@localhost>
2013-02-17 10:59:27 +0800

    added implementation of Capability entity CRUD, and ignored derby.log

--- rdonasco-security-app/src/test/java/com/rdonasco/security/CapabilityManagerJPATest.java
+++ rdonasco-security-app/src/test/java/com/rdonasco/security/CapabilityManagerJPATest.java
@@ -19,67 +19,68 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import test.util.ArchiveCreator;
 import static org.junit.Assert.*;
+
 /**
  * Unit test for simple App.
  */
 @RunWith(Arquillian.class)
-public class CapabilityManagerJPATest 
+public class CapabilityManagerJPATest
 {
-	@EJB 
-	private CapabilityManagerRemote capabilityManager; 
-	
-    @Deployment
-    public static JavaArchive createTestArchive()
-    {
-		JavaArchive  archive = ArchiveCreator.createCommonArchive()
+
+	@EJB
+	private CapabilityManagerRemote capabilityManager;
+
+	@Deployment
+	public static JavaArchive createTestArchive()
+	{
+		JavaArchive archive = ArchiveCreator.createCommonArchive()
 				.addPackage(ActionDAO.class.getPackage())
 				.addPackage(SystemSecurityManagerRemote.class.getPackage())
 				.addPackage(ActionVO.class.getPackage())
-				.addPackage(Action.class.getPackage())
-				;
-		
+				.addPackage(Action.class.getPackage());
+
 		return archive;
 	}
 
 	@Test
 	public void testAddAction() throws Exception
-    {
+	{
 		ActionVO action = new ActionVO();
 		action.setDescription("test");
 		action.setName("test");
 		ActionVO savedAction = capabilityManager.createNewAction(action);
 		assertNotNull(savedAction.getId());
-		
-    }
-	
+
+	}
+
 	@Test
 	public void testEditAction() throws Exception
-    {
+	{
 		System.out.println("editAction");
 		final String actionName = "edit";
 		ActionVO savedAction = createTestDataActionNamed(actionName);
 		ActionVO foundAction = capabilityManager.findActionNamed(actionName);
 		assertNotNull(foundAction);
-		assertEquals(savedAction.getId(),foundAction.getId());
-		assertEquals(savedAction.getDescription(),foundAction.getDescription());
+		assertEquals(savedAction.getId(), foundAction.getId());
+		assertEquals(savedAction.getDescription(), foundAction.getDescription());
 		foundAction.setDescription("edited action");
-		capabilityManager.updateAction(foundAction); 
+		capabilityManager.updateAction(foundAction);
 		ActionVO updatedAction = capabilityManager.findActionNamed(actionName);
 		assertNotNull(foundAction);
-		assertEquals(foundAction.getId(),updatedAction.getId());
-		assertEquals(foundAction.getDescription(),updatedAction.getDescription());		
-    }	
-	
-	@Test(expected=NonExistentEntityException.class)
+		assertEquals(foundAction.getId(), updatedAction.getId());
+		assertEquals(foundAction.getDescription(), updatedAction.getDescription());
+	}
+
+	@Test(expected = NonExistentEntityException.class)
 	public void testDeleteAction() throws Exception
 	{
 		System.out.println("deleteAction");
 		ActionVO actionToDelete = createTestDataActionNamed("actionToDelete");
 		capabilityManager.removeAction(actionToDelete);
 		capabilityManager.findActionNamed(actionToDelete.getName());
-		
+
 	}
-	
+
 	@Test
 	public void testAddResource() throws Exception
 	{
@@ -88,7 +89,7 @@ public class CapabilityManagerJPATest
 		assertNotNull(resourceToAdd);
 		assertNotNull(resourceToAdd.getId());
 	}
-	
+
 	@Test
 	public void testEditResource() throws Exception
 	{
@@ -98,19 +99,19 @@ public class CapabilityManagerJPATest
 		resourceToEdit.setDescription(editedDescription);
 		capabilityManager.updateResource(resourceToEdit);
 		ResourceVO updatedResource = capabilityManager.findResourceNamedAs(resourceToEdit.getName());
-		assertEquals(resourceToEdit,updatedResource);
-		assertEquals(resourceToEdit.getDescription(),updatedResource.getDescription());
+		assertEquals(resourceToEdit, updatedResource);
+		assertEquals(resourceToEdit.getDescription(), updatedResource.getDescription());
 	}
-	
-	@Test(expected=NonExistentEntityException.class)
+
+	@Test(expected = NonExistentEntityException.class)
 	public void testRemoveResource() throws Exception
 	{
 		System.out.println("removeResource");
 		ResourceVO resourceToRemove = createTestDataResourceNamed("resourceToRemove");
 		capabilityManager.removeResource(resourceToRemove);
-		capabilityManager.findResourceNamedAs(resourceToRemove.getName());		
+		capabilityManager.findResourceNamedAs(resourceToRemove.getName());
 	}
-	
+
 	@Test
 	public void testAddCapability() throws Exception
 	{
@@ -119,27 +120,41 @@ public class CapabilityManagerJPATest
 		final String resourceName = "employee";
 		CapabilityVO savedCapabilityVO = createTestDataCapabilityWithActionAndResourceName(actionName, resourceName);
 		assertNotNull(savedCapabilityVO);
-		assertNotNull(savedCapabilityVO.getId());		
+		assertNotNull(savedCapabilityVO.getId());
 	}
-	
+	private static int KEY = 0;
+
 	@Test
 	public void testUpdateCapability() throws Exception
 	{
 		System.out.println("updateCapability");
-		final String actionName = "add";
-		final String resourceName = "employee";
+		final String actionName = "add" + (KEY++);
+		final String resourceName = "employee" + (KEY++);
 		CapabilityVO capabilityVOtoUpdate = createTestDataCapabilityWithActionAndResourceName(actionName, resourceName);
 		capabilityVOtoUpdate.setDescription("updated description");
 		capabilityVOtoUpdate.setTitle("updated title");
 		capabilityManager.updateCapability(capabilityVOtoUpdate);
 		CapabilityVO updatedCapabilityVO = capabilityManager.findCapabilityWithId(capabilityVOtoUpdate.getId());
-		assertEquals(capabilityVOtoUpdate.getId(),updatedCapabilityVO.getId());
-		assertEquals(capabilityVOtoUpdate.getDescription(),updatedCapabilityVO.getDescription());
-		assertEquals(capabilityVOtoUpdate.getTitle(),updatedCapabilityVO.getTitle());
-		assertEquals(capabilityVOtoUpdate.getActions().size(),updatedCapabilityVO.getActions().size());
+		assertEquals(capabilityVOtoUpdate.getId(), updatedCapabilityVO.getId());
+		assertEquals(capabilityVOtoUpdate.getDescription(), updatedCapabilityVO.getDescription());
+		assertEquals(capabilityVOtoUpdate.getTitle(), updatedCapabilityVO.getTitle());
+		assertEquals(capabilityVOtoUpdate.getActions().size(), updatedCapabilityVO.getActions().size());
 	}
 
-	private ActionVO createTestDataActionNamed(String name) throws CapabilityManagerException
+	@Test
+	public void testDeleteCapability() throws Exception
+	{
+		System.out.println("deleteCapability");
+		final String actionName = "delete" + (KEY++);
+		final String resourceName = "employee" + (KEY++);
+		CapabilityVO capabilityVOtoDelete = createTestDataCapabilityWithActionAndResourceName(actionName, resourceName);
+		capabilityManager.removeCapability(capabilityVOtoDelete);
+		CapabilityVO foundCapabilityVO = capabilityManager.findCapabilityWithId(capabilityVOtoDelete.getId());
+		assertNull(foundCapabilityVO);
+	}
+
+	private ActionVO createTestDataActionNamed(String name) throws
+			CapabilityManagerException
 	{
 		ActionVO action = new ActionVO();
 		action.setDescription(name + " description");
@@ -148,18 +163,20 @@ public class CapabilityManagerJPATest
 		return savedAction;
 	}
 
-	private ResourceVO createTestDataResourceNamed(String name) throws CapabilityManagerException
+	private ResourceVO createTestDataResourceNamed(String name) throws
+			CapabilityManagerException
 	{
 		ResourceVO resourceToAdd = new ResourceVOBuilder()
 				.setName(name)
 				.setDescription(name + "description")
 				.createResourceVO();
-		
+
 		ResourceVO resourceAdded = capabilityManager.addResource(resourceToAdd);
 		return resourceAdded;
 	}
 
-	private CapabilityVO createTestDataCapabilityWithActionAndResourceName(final String actionName,
+	private CapabilityVO createTestDataCapabilityWithActionAndResourceName(
+			final String actionName,
 			final String resourceName) throws CapabilityManagerException
 	{
 		ActionVO action = createTestDataActionNamed(actionName);
