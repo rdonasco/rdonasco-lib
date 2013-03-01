c3e8fb67e47fdd3670d786fc62089313c19791f3
rdonasco <devnull@localhost>
2013-03-01 22:32:59 +0800

    added feature to ensure that configdata.xpath is also populated when we convert xpath to elements

--- rdonasco-config/src/main/java/com/rdonasco/config/util/XpathToConfigTransformer.java
+++ rdonasco-config/src/main/java/com/rdonasco/config/util/XpathToConfigTransformer.java
@@ -21,6 +21,8 @@ import java.util.StringTokenizer;
 import com.rdonasco.config.data.ConfigAttribute;
 import com.rdonasco.config.data.ConfigData;
 import com.rdonasco.config.data.ConfigElement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -29,27 +31,62 @@ import com.rdonasco.config.data.ConfigElement;
 public class XpathToConfigTransformer
 {
 
-    public static List<ConfigData> transform(String xpath)
-    {        
-        StringTokenizer tokenizer = new StringTokenizer(xpath,"/");
-        int tokens = tokenizer.countTokens();
-        List<ConfigData> configList = new ArrayList<ConfigData>(tokens);
-        ConfigElement element = null;
-        for(int i = 0; i < tokens - 1; i++)
-        {
-           ConfigElement parent = element;
-           element = new ConfigElement();
-           element.setName(tokenizer.nextToken());
-           if(parent != null)
-           {
-               parent.addSubConfig(element);
-           }
-           configList.add(element);
-        }
-        ConfigAttribute attribute = new ConfigAttribute();
-        attribute.setName(tokenizer.nextToken());
-        element.addConfigAttribute(attribute);
-        configList.add(attribute);
-        return configList;
-    }
+	private static final Logger LOG = Logger.getLogger(XpathToConfigTransformer.class.getName());
+
+	public static List<ConfigData> transform(String xpath)
+	{
+		StringTokenizer tokenizer = new StringTokenizer(xpath, "/");
+		int tokens = tokenizer.countTokens();
+		List<ConfigData> configList = new ArrayList<ConfigData>(tokens);
+		ConfigElement element = null;
+		for (int i = 0; i < tokens - 1; i++)
+		{
+			ConfigElement parent = element;
+			element = new ConfigElement();
+			element.setName(tokenizer.nextToken());
+			if (parent != null)
+			{
+				parent.addSubConfig(element);
+			}
+			configList.add(element);
+			configureXpath(element);
+
+		}
+		ConfigAttribute attribute = new ConfigAttribute();
+		attribute.setName(tokenizer.nextToken());
+		attribute.setParentConfig(element);
+		configureXpath(attribute);
+		if(element != null)
+		{
+			element.addConfigAttribute(attribute);
+		}
+		configList.add(attribute);
+		return configList;
+	}
+
+	public static ConfigData configureXpath(ConfigData configData)
+	{
+		try
+		{
+			StringBuilder xPathBuilder = new StringBuilder();
+			if (null != configData && null != configData.getParentConfig())
+			{
+				if (null == configData.getParentXpath() || !configData.getParentXpath().startsWith("/"))
+				{
+					configureXpath(configData.getParentConfig());
+				}
+				xPathBuilder.append(configData.getParentXpath());
+			}
+			if (null != configData)
+			{
+				xPathBuilder.append("/").append(configData.getName());
+				configData.setXpath(xPathBuilder.toString());
+			}
+		}
+		catch (Exception e)
+		{
+			LOG.log(Level.FINE,e.getMessage(),e);
+		}
+		return configData;
+	}
 }
