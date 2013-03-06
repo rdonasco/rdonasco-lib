8d879627d00d4a5297b8ac8c6a6f4ad05dfb6ca1
rdonasco <devnull@localhost>
2013-03-06 09:31:26 +0800

    added test for password encryption

---
+++ rdonasco-security-client/src/test/java/com/rdonasco/security/utils/EncryptionUtilTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2013 Roy F. Donasco.
+ * File Created on: 06-Mar-2013
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.rdonasco.security.utils;
+
+import javax.crypto.SecretKeyFactory;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Roy F. Donasco
+ */
+public class EncryptionUtilTest
+{
+	
+	public EncryptionUtilTest()
+	{
+	}
+	
+	@BeforeClass
+	public static void setUpClass()
+	{
+	}
+	
+	@AfterClass
+	public static void tearDownClass()
+	{
+	}
+	
+	@Before
+	public void setUp()
+	{
+	}
+	
+	@After
+	public void tearDown()
+	{
+	}
+
+	/**
+	 * Test of encryptWithPassword method, of class EncryptionUtil.
+	 */
+	@Test
+	public void testEncryptWithPassword() throws Exception
+	{
+		System.out.println("encryptWithPassword");
+		String stringToEncrypt = "cool password of you know who";
+		String password = "password to encrypt";
+		String result = EncryptionUtil.encryptWithPassword(stringToEncrypt, password);
+		System.out.println("result = " + result);
+		assertNotSame(stringToEncrypt, result);
+	}
+
+	/**
+	 * Test of decryptWithPassword method, of class EncryptionUtil.
+	 */
+	@Test
+	public void testDecryptWithPassword() throws Exception
+	{
+		System.out.println("decryptWithPassword");
+		String encryptedString = "CcodRLCmRG904cla/U374NYVvCk75Z1UI+kpD8H5fVk=";
+		String password = "password to encrypt";
+		String expResult = "cool password of you know who";
+		String result = EncryptionUtil.decryptWithPassword(encryptedString, password);
+		System.out.println("result = " + result);
+		assertEquals(expResult, result);
+	}
+}
