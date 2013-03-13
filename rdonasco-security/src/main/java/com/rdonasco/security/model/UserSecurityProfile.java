cfdbe92c3efb1bdc92199b9becf2831c915b6548
rdonasco <devnull@localhost>
2013-03-13 08:28:10 +0800

    corrected method and removed warning

--- rdonasco-security/src/main/java/com/rdonasco/security/model/UserSecurityProfile.java
+++ rdonasco-security/src/main/java/com/rdonasco/security/model/UserSecurityProfile.java
@@ -146,7 +146,6 @@ public class UserSecurityProfile implements Serializable
 	@Override
 	public boolean equals(Object object)
 	{
-		// TODO: Warning - this method won't work in the case the id fields are not set
 		boolean isEqual = true;
 		if (!(object instanceof UserSecurityProfile))
 		{
