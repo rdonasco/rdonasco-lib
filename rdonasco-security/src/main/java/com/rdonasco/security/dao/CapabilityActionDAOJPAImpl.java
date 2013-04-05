906e04b373bcfc1fbf830eb7e88fb543aaa84416
rdonasco <devnull@localhost>
2013-04-05 08:56:51 +0800

    added persistence unit and added new DAO

---
+++ rdonasco-security/src/main/java/com/rdonasco/security/dao/CapabilityActionDAOJPAImpl.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2013 Roy F. Donasco.
+ * File Created on: 04-Apr-2013
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.rdonasco.security.dao;
+
+import com.rdonasco.security.model.CapabilityAction;
+
+/**
+ *
+ * @author Roy F. Donasco
+ */
+public class CapabilityActionDAOJPAImpl extends AbstractSecurityDAO<CapabilityAction>
+		implements CapabilityActionDAO
+{
+
+	@Override
+	public Class<CapabilityAction> getDataClass()
+	{
+		return CapabilityAction.class;
+	}
+}
