f94225fb5d17f12709d9edc5627c2a8fa059ef40
rdonasco <devnull@localhost>
2013-04-09 16:57:43 +0800

    linked settings button to the configuration view

--- security-app/security-app-web/src/main/java/com/rdonasco/security/home/controllers/SettingsHomeButtonController.java
+++ security-app/security-app-web/src/main/java/com/rdonasco/security/home/controllers/SettingsHomeButtonController.java
@@ -8,12 +8,17 @@ import com.rdonasco.common.exceptions.WidgetException;
 import com.rdonasco.common.i18.I18NResource;
 import com.rdonasco.common.vaadin.controller.ApplicationExceptionPopupProvider;
 import com.rdonasco.common.vaadin.controller.ApplicationPopupProvider;
+import com.rdonasco.config.view.ConfigDataView;
+import com.rdonasco.config.view.ConfigDataViewController;
 import com.rdonasco.security.app.themes.SecurityDefaultTheme;
 import com.rdonasco.security.home.views.FeatureHomeButton;
+import com.vaadin.terminal.Sizeable;
 import com.vaadin.terminal.ThemeResource;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.Panel;
 import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 
 /**
@@ -24,12 +29,19 @@ public class SettingsHomeButtonController implements
 		HomeViewButtonController
 {
 
+	private static final long serialVersionUID = 1L;
+	private static final String CONSTANTS_SETTINGS = "Settings";
 	@Inject
 	private FeatureHomeButton featureButton;
 	@Inject
 	private ApplicationExceptionPopupProvider exceptionPopupProvider;
 	@Inject
 	private ApplicationPopupProvider popupProvider;
+	@Inject
+	private Instance<ConfigDataViewController> configDataViewControllerProvider;
+	@Inject
+	private Instance<HomeFrameViewController> homeFrameViewControllers;
+	private ConfigDataView configView;
 
 	@PostConstruct
 	@Override
@@ -43,7 +55,7 @@ public class SettingsHomeButtonController implements
 				@Override
 				public void buttonClick(Button.ClickEvent event)
 				{
-					popupProvider.popUpInfo("Settings Clicked");
+					homeFrameViewControllers.get().setWorkspaceContent(getWorkspaceContent());
 				}
 			});
 		}
@@ -62,12 +74,30 @@ public class SettingsHomeButtonController implements
 	@Override
 	public void refreshView() throws WidgetException
 	{
-		featureButton.setCaption(I18NResource.localize("Settings"));
+		featureButton.setCaption(I18NResource.localize(CONSTANTS_SETTINGS));
 		featureButton.setIcon(new ThemeResource(SecurityDefaultTheme.ICONS_32x32_SETTINGS));
 	}
 
 	private void doTheRefresh() throws WidgetException
 	{
 		refreshView();
+	}
+
+	private ConfigDataView getConfigView()
+	{
+		if (null == configView)
+		{
+			configView = configDataViewControllerProvider.get().getControlledView();
+			configView.setHeight(400f, Sizeable.UNITS_PIXELS);
+		}
+		return configView;
+	}
+
+	private Component getWorkspaceContent()
+	{
+		Panel panel = new Panel(I18NResource.localize(CONSTANTS_SETTINGS));
+		panel.setStyleName(SecurityDefaultTheme.CSS_PANEL_BUBBLE);
+		panel.addComponent(getConfigView());
+		return panel;
 	}
 }
