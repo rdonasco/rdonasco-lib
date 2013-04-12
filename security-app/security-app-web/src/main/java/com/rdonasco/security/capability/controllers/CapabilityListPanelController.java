998f9213493f3adc40a6842a823f0d710890f38e
rdonasco <devnull@localhost>
2013-04-12 08:47:32 +0800

    removed unused method

--- security-app/security-app-web/src/main/java/com/rdonasco/security/capability/controllers/CapabilityListPanelController.java
+++ security-app/security-app-web/src/main/java/com/rdonasco/security/capability/controllers/CapabilityListPanelController.java
@@ -13,7 +13,7 @@ import com.rdonasco.common.vaadin.controller.ApplicationExceptionPopupProvider;
 import com.rdonasco.common.vaadin.controller.ApplicationPopupProvider;
 import com.rdonasco.security.app.themes.SecurityDefaultTheme;
 import com.rdonasco.security.capability.utils.Constants;
-import com.rdonasco.security.capability.utils.TableHelper;
+import com.rdonasco.datamanager.utils.TableHelper;
 import com.rdonasco.security.capability.views.CapabilityListPanel;
 import com.rdonasco.security.capability.vo.CapabilityItemVO;
 import com.rdonasco.security.capability.vo.CapabilityItemVOBuilder;
@@ -171,34 +171,6 @@ public class CapabilityListPanelController implements
 			popupProvider = popupProviderFactory.get();
 		}
 		return popupProvider;
-	}
-
-	private CapabilityItemVO createDummyAddRecord()
-	{
-		String caption = I18NResource.localize("Add new capability");
-		CapabilityVO capability = new CapabilityVOBuilder()
-				.setId(ADD_CAPABILITY_ID)
-				.setTitle(caption)
-				.createCapabilityVO();
-		CapabilityItemVO itemVO;
-		Embedded icon = new Embedded(null, new ThemeResource(SecurityDefaultTheme.ICONS_16x16_ADD));
-		icon.addListener(new MouseEvents.ClickListener()
-		{
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public void click(MouseEvents.ClickEvent event)
-			{
-				addNewCapability();
-			}
-		});
-		icon.setDescription(caption);
-		final CapabilityItemVO capabilityItem = new CapabilityItemVOBuilder()
-				.setCapabilityVO(capability)
-				.setEmbeddedIcon(icon)
-				.createCapabilityItemVO();
-		itemVO = capabilityItem;
-		return itemVO;
 	}
 
 	private void addNewCapability()
