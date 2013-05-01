ab0323e1a7f694177cb5a57026ee9f8a4a0a7251
rdonasco <devnull@localhost>
2013-05-01 09:01:01 +0800

    started working on userRole

---
+++ rdonasco-security/src/main/java/com/rdonasco/security/model/UserRole.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2013 Roy F. Donasco.
+ * File Created on: 27-Jan-2013
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.rdonasco.security.model;
+
+import com.rdonasco.security.utils.SecurityConstants;
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.TableGenerator;
+
+/**
+ *
+ * @author Roy F. Donasco
+ */
+@Entity
+@Table(name = "user_role")
+public class UserRole implements Serializable
+{
+
+	private static final long serialVersionUID = 1L;
+
+	private static final String GENERATOR_KEY = "ID_IDGEN";
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.TABLE, generator = GENERATOR_KEY)
+	@TableGenerator(name = GENERATOR_KEY, table = SecurityConstants.TABLE_SEQUENCE)
+	@Column(name = "id", nullable = false)
+	private Long id;
+
+	@Basic(optional = false)
+	@Column(name = "name", nullable = false, length = 256, unique = true)
+	private String name;
+
+	public Long getId()
+	{
+		return id;
+	}
+
+	public void setId(Long id)
+	{
+		this.id = id;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode()
+	{
+		int hash = 7;
+		hash = 37 * hash + (this.id != null ? this.id.hashCode() : 0);
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		boolean isEqual = true;
+		if (obj == null)
+		{
+			isEqual = false;
+		}
+		else if (getClass() != obj.getClass())
+		{
+			isEqual = false;
+		}
+		else
+		{
+			final UserRole other = (UserRole) obj;
+			if (this.id != other.id && (this.id == null || !this.id.equals(other.id)))
+			{
+				isEqual = false;
+			}
+		}
+		return isEqual;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "UserRole{" + "id=" + id + ", name=" + name + '}';
+	}
+}
