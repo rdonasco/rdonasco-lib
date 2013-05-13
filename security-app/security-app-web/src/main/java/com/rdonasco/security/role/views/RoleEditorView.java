8cc117e47bf28c2139bda5fa98d72b11075d51ab
rdonasco <devnull@localhost>
2013-05-13 18:05:01 +0800

    partially completed role editor layout

--- security-app/security-app-web/src/main/java/com/rdonasco/security/role/views/RoleEditorView.java
+++ security-app/security-app-web/src/main/java/com/rdonasco/security/role/views/RoleEditorView.java
@@ -14,14 +14,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.rdonasco.security.role.views;
 
 import com.rdonasco.common.exceptions.WidgetInitalizeException;
 import com.rdonasco.common.i18.I18NResource;
 import com.rdonasco.common.vaadin.view.ControlledView;
 import com.rdonasco.security.app.themes.SecurityDefaultTheme;
+import com.vaadin.terminal.ThemeResource;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Panel;
+import com.vaadin.ui.Table;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 
 /**
@@ -30,26 +34,85 @@ import com.vaadin.ui.VerticalLayout;
  */
 public class RoleEditorView extends VerticalLayout implements ControlledView
 {
+
 	private static final long serialVersionUID = 1L;
 
 	private Panel roleDetailPanel = new Panel();
 
 	private Panel roleCapabilitiesPanel = new Panel();
+
+	private Button saveButton = new Button();
+
+	private Button editButton = new Button();
+
+	private Button cancelButton = new Button();
+
+	private TextField nameField = new TextField(I18NResource.localize("Name"));
+
+	private HorizontalLayout buttonLayout = new HorizontalLayout();
+
+	private Table roleCapabilitiesTable = new Table();
+
 	public RoleEditorView()
 	{
-
 	}
+
+	public Table getRoleCapabilitiesTable()
+	{
+		return roleCapabilitiesTable;
+	}
+
 
 	@Override
 	public void initWidget() throws WidgetInitalizeException
 	{
-		roleDetailPanel.setCaption(I18NResource.localize("Role Editor"));
-		roleDetailPanel.setStyleName(SecurityDefaultTheme.CSS_PANEL_BUBBLE);
-		roleCapabilitiesPanel.setCaption(I18NResource.localize("Capabilities of this role"));
-		roleCapabilitiesPanel.setStyleName(SecurityDefaultTheme.CSS_PANEL_BUBBLE);
-		setSpacing(true);
+		configureRoleFields();
+		configureRoleCapabilityFields();
+		configureEditorButtons();
 
 		addComponent(roleDetailPanel);
 		addComponent(roleCapabilitiesPanel);
+		addComponent(buttonLayout);
+
+		setExpandRatio(roleCapabilitiesPanel, 1);
+		addStyleName(SecurityDefaultTheme.CSS_ROLE_EDITOR);
+		setSpacing(true);
+
 	}
+
+	private void configureRoleFields()
+	{
+		roleDetailPanel.setCaption(I18NResource.localize("Role Editor"));
+		roleDetailPanel.setStyleName(SecurityDefaultTheme.CSS_PANEL_BUBBLE);
+		roleDetailPanel.addComponent(nameField);
+	}
+
+	private void configureRoleCapabilityFields()
+	{
+		roleCapabilitiesPanel.setCaption(I18NResource.localize("Capabilities of this role"));
+		roleCapabilitiesPanel.setStyleName(SecurityDefaultTheme.CSS_PANEL_BUBBLE);
+		roleCapabilitiesPanel.addComponent(getRoleCapabilitiesTable());
+		getRoleCapabilitiesTable().addStyleName(SecurityDefaultTheme.CSS_TABLE_SMALL_STRIPED);
+		getRoleCapabilitiesTable().addStyleName(SecurityDefaultTheme.CSS_TABLE_BORDERLESS);
+		getRoleCapabilitiesTable().setSizeFull();
+		((VerticalLayout) roleCapabilitiesPanel.getContent()).setExpandRatio(getRoleCapabilitiesTable(), 1);
+	}
+
+	private void configureEditorButtons()
+	{
+		editButton.setCaption(I18NResource.localize("Edit"));
+		editButton.setIcon(new ThemeResource(SecurityDefaultTheme.ICONS_16x16_EDIT));
+		saveButton.setCaption(I18NResource.localize("Save"));
+		saveButton.setIcon(new ThemeResource(SecurityDefaultTheme.ICONS_16x16_SAVE));
+		cancelButton.setCaption(I18NResource.localize("Cancel"));
+		cancelButton.setIcon(new ThemeResource(SecurityDefaultTheme.ICONS_16x16_CANCEL));
+
+		saveButton.addStyleName(SecurityDefaultTheme.CSS_BUTTON_DEFAULT);
+		buttonLayout.addComponent(editButton);
+		buttonLayout.addComponent(saveButton);
+		buttonLayout.addComponent(cancelButton);
+		buttonLayout.setSpacing(true);
+	}
+
+
 }
