2cc3642a4586aa7b81fffcc528eea7f1da0d13fc
rdonasco <devnull@localhost>
2013-05-16 15:51:55 +0800

    added serial

--- rdonasco-common/src/main/java/com/rdonasco/common/exceptions/NonExistentEntityException.java
+++ rdonasco-common/src/main/java/com/rdonasco/common/exceptions/NonExistentEntityException.java
@@ -18,6 +18,7 @@ package com.rdonasco.common.exceptions;
 
 public class NonExistentEntityException extends Exception
 {
+	private static final long serialVersionUID = 1L;
 
     private Object entity;
 
