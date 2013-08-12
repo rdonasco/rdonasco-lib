a8f595c44c6ad209bf648ecb9ba54a6c052310f3
Roy Donasco <rdonasco2@yahoo.com>
2013-08-12 12:24:21 +0800

    removed unused lines

--- security-app/rdonasco-security/src/test/java/com/rdonasco/security/services/SystemSecurityInitializerLocalTest.java
+++ security-app/rdonasco-security/src/test/java/com/rdonasco/security/services/SystemSecurityInitializerLocalTest.java
@@ -119,29 +119,6 @@ public class SystemSecurityInitializerLocalTest
 		}
 	}
 
-	/*
-	 @Test
-	 public void testInitializeDefaultSystemAccessCapabilities() throws Exception
-	 {
-	 System.out.println("initializeDefaultSystemAccessCapabilities");
-	 systemSecurityInitializer.initializeDefaultSystemAccessCapabilities();
-	 String title;
-	 String resourceName;
-	 String action;
-	 for (String[] capabilityInitString : SystemSecurityInitializerLocal.DEFAULT_CAPABILITY_ELEMENTS)
-	 {
-	 title = capabilityInitString[SystemSecurityInitializerLocal.ELEMENT_CAPABILITY_TITLE];
-	 CapabilityVO capability = capabilityManager.findCapabilityWithTitle(title);
-	 assertNotNull("capability" + title + " not found",capability);
-	 //			resourceName = capabilityInitString[SystemSecurityInitializerLocal.ELEMENT_RESOURCE];
-	 //			for (int i = SystemSecurityInitializerLocal.ELEMENT_RESOURCE + 1; i < capabilityInitString.length; i++)
-	 //			{
-	 //				action = capabilityInitString[i];
-	 //			}
-	 }
-
-	 }
-	 */
 	// ------ utility methods below here ------ //
 	private ActionVO createTestDataActionNamed(String name) throws
 			CapabilityManagerException
