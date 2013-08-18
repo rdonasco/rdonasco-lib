06807a9b31dbb3eaade9924ac0fef207b468e33f
Roy Donasco <rdonasco2@yahoo.com>
2013-08-18 19:52:57 +0800

    removed unnecessary imports

--- security-app/SimpleSecuredWeb01/src/main/java/com/rdonasco/sample/SampleLoggedOnSessionProvider.java
+++ security-app/SimpleSecuredWeb01/src/main/java/com/rdonasco/sample/SampleLoggedOnSessionProvider.java
@@ -4,19 +4,9 @@
  */
 package com.rdonasco.sample;
 
-import com.rdonasco.config.services.ConfigDataManagerVODecoratorRemote;
-import com.rdonasco.security.authentication.factories.LogonServiceFactory;
-import com.rdonasco.security.authentication.services.DefaultLogonService;
 import com.rdonasco.security.authentication.services.LoggedOnSession;
 import com.rdonasco.security.authentication.services.LoggedOnSessionProvider;
-import com.rdonasco.security.authentication.services.LogonService;
-import com.rdonasco.security.exceptions.LogonServiceNotFoundException;
-import com.rdonasco.security.vo.LogonVO;
-import com.rdonasco.security.vo.UserSecurityProfileVO;
 import java.io.Serializable;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 
